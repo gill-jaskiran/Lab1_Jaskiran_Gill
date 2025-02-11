b4c838328abd2bc356ed1e3ab08ed7714a45b3e2
gill-jaskiran <jaskirangill14@gmail.com>
2025-02-11 13:05:21 -0500

    Added Title

--- Lab1_Jaskiran_Gill/ContentView.swift
+++ Lab1_Jaskiran_Gill/ContentView.swift
@@ -2,20 +2,13 @@
 //  ContentView.swift
 //  Lab1_Jaskiran_Gill
 //
-//  Created by Bandhna Gill on 2025-02-11.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Prime Game") // Adding Game Title
     }
 }
 
--- Lab1_Jaskiran_Gill/Lab1_Jaskiran_GillApp.swift
+++ Lab1_Jaskiran_Gill/Lab1_Jaskiran_GillApp.swift
@@ -2,7 +2,6 @@
 //  Lab1_Jaskiran_GillApp.swift
 //  Lab1_Jaskiran_Gill
 //
-//  Created by Bandhna Gill on 2025-02-11.
 //
 
 import SwiftUI
