9c61e3b2bbaf840a1cc60a91b477c61bf2774ab4
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2025-02-11 14:02:24 -0500

    Adding Timer

--- Lab1_Jaskiran_Gill/ContentView.swift
+++ Lab1_Jaskiran_Gill/ContentView.swift
@@ -8,6 +8,10 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var currentNumber = Int.random(in: 1...100)
+    @State private var numOfCorrectAnswers = 0
+    @State private var numOfWrongAnswers = 0
+    @State private var numOfAttempts = 0
+    @State private var timer: Timer?
     
     var body: some View {
         VStack {
@@ -15,16 +19,26 @@ struct ContentView: View {
                 .font(.largeTitle) // Added UI
                 .foregroundColor(.pink)
                 .padding(.top, 40)
+                .frame(maxWidth: .infinity, alignment: .top)
             Text("Is this a Prime Number?")
-                            .font(.title)
-                            .padding(.top, 10)
+                .font(.title)
+                .padding(.top, 10)
             Text("\(currentNumber)") //a adding the heading and UI
-                       .font(.system(size: 90, weight: .bold))
-                       .padding(.bottom, 20)
+                .font(.system(size: 90, weight: .bold))
+                .padding(.bottom, 0)
+            
+            }
+            
+        }
+        
+        func startTimer() {
+            timer?.invalidate()
+            timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
+                numOfWrongAnswers += 1
+                numOfAttempts += 1
+            }
         }
     }
-}
-
 #Preview {
     ContentView()
 }
