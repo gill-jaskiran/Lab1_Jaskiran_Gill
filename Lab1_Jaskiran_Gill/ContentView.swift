9c1f643c9b02ec6b03afa70be03e80b6a1310b39
gill-jaskiran <jaskirangill14@gmail.com>
2025-02-11 12:54:41 -0500

    Initial Commit

---
+++ Lab1_Jaskiran_Gill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Jaskiran_Gill
+//
+//  Created by Bandhna Gill on 2025-02-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Jaskiran_Gill/Lab1_Jaskiran_GillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Jaskiran_GillApp.swift
+//  Lab1_Jaskiran_Gill
+//
+//  Created by Bandhna Gill on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Jaskiran_GillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
